7140cc0abc4dbdedf4528e2450241ca6d4861038
Kenny Patrick <kennypatrick96@gmail.com>
2023-08-16 17:33:22 -0600

    added tests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,41 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  if (!request.body.title || !request.body.url) {
+    response.status(400).end()
+  } else {
+    if (!request.body.likes) {
+      request.body.likes = 0
+    }
+    const blog = new Blog(request.body)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  }
+})
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    likes: body.likes,
+    url: body.url
+  }
+
+  const savedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(200).json(savedBlog)
 })
 
 module.exports = blogsRouter
---
+++ utils/list_helper.js
@@ -0,0 +1,73 @@
+const lodash = require('lodash')
+const logger = require('./logger')
+
+const dummy = (blogs) => {
+  logger.info(`Running dummy with blogs: ${blogs}`)
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((accumulator, currentBlog) => {
+    return accumulator + currentBlog.likes
+  }, 0)
+}
+
+const simplifyFavoriteBlogs = (blogs) => {
+  return blogs.map(blog => {
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes
+    }
+  })
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  const simplifiedBlogs = simplifyFavoriteBlogs(blogs)
+  return simplifiedBlogs.reduce((accumulator, currentBlog) => {
+    if (currentBlog.likes > accumulator.likes) {
+      return currentBlog
+    }
+    return accumulator
+  }, { likes: -Infinity })
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  const groupedByAuthor = lodash.groupBy(blogs, 'author')
+  const longestGroup = lodash.maxBy(Object.values(groupedByAuthor), group => group.length)
+  console.log(longestGroup)
+  return {
+    author: longestGroup[0].author,
+    blogs: longestGroup.length
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+  const groupedByAuthor = lodash.groupBy(blogs, 'author')
+  const likesByAuthor = lodash.mapValues(groupedByAuthor, group => {
+    return lodash.sumBy(group, 'likes')
+  })
+  const mostLikedAuthor = lodash.maxBy(lodash.toPairs(likesByAuthor), pair => pair[1])
+
+  return {
+    author: mostLikedAuthor[0],
+    likes: mostLikedAuthor[1]
+  }
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
